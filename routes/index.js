c93b3103c3e93f008387f116ba41cee4c08118d2
P-Wall86 <sisterwall@gmail.com>
2025-09-18 17:31:27 -0300

    Mongo Connect

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get ('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Te manda saludos el chancho peludo.');
 });
 
 module.exports = router;
