78ebe2de40f3e39e33d195dfc18d1fada8442c76
P-Wall86 <sisterwall@gmail.com>
2025-09-27 17:55:50 -0300

    rollback ffs

--- server.js
+++ server.js
@@ -13,41 +13,37 @@ const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
 
 app
-	.use(bodyParser.json())
-	.use(session ({
-	secret: "secret",
-resave: false,
-saveUninitialized: true
-}))
+    .use(
+        cors({ origin: "*", methods: ["GET", "POST", "PUT", "PATCH", "DELETE"] })
+    )
+    .use(express.json())
+    .use(express.urlencoded({ extended: true }))
+    .use(bodyParser.json())
+    .use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-.use(passport.initialize())
-
-.use(passport.session())
-
-.use((req, res, next) => {
-res.setHeader("Access-Controll-Allow-Origin", "*");
-res.setHeader(
-"Access-Control-Allow-Headers",
-"Origin, X-Requested-with, Content-Type, Accept, 2-Key, Authorization"
+app.use(
+    session({
+        secret: "secret",
+        resave: false,
+        saveUninitialized: true,
+    })
 );
-res.setHeader(
-"Access-Control-Allow-Methods", "POST, GET, PUT, PATCH, OPTIONS, DELETE"
-);
-next();
-})
-.use(cors({methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']}))
-.use(cors({origin: '*'}))
- .use('/', require('./routes/index.js'));
 
-passport.use(new GitHubStrategy({
-clientID: process.env.GITHUB_CLIENT_ID,
-clientSecret: process.env.GITHUB_CLIENT_SECRET,
-callbackURL: process.env.CALLBACK_URL
-},
-function(accessToken, refreshToken, profile, done){
-return done (null, profile);
-}
-));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+    new GitHubStrategy(
+        {
+            clientID: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
+            callbackURL: process.env.CALLBACK_URL,
+        },
+        function (accessToken, refreshToken, profile, done) {
+            return done(null, profile);
+        }
+    )
+);
 
 passport.serializeUser((user, done) => {
     done(null, user);
@@ -56,15 +52,16 @@ passport.deserializeUser((obj, done) => {
     done(null, obj);
 });
 
-app.get('/', (req, res) => {res.send}(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : "Logged Out")});
+app.get('/', (req, res) => {res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : "Logged Out")});
 
 app.get('/github/callback', passport.authenticate('github', {
-failureRedirect: '/api-docs', session: false}),
+failureRedirect: '/api-docs'}),
 (req, res) => {
 req.session.user = req.user,
 res.redirect ('/');
 });
 
+app.use("/", require("./routes/"));
 
 mongodb.initDb((err) => {
     console.log("Database initialized!");
